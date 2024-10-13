39f0d9d0c507d6362af5c5bc65acebaff520790b
Abdou-Jabar Mikailou <151407343+Abdou-Jabar@users.noreply.github.com>
2024-10-13 20:27:46 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
-<?php
-
-?>
-<html lang="en">
+<!DOCTYPE html>
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -14,13 +12,15 @@
             <h1 class="text-2xl font-bold">Systeme de compteur de vue</h1>
         </div>
     </header>
+    <!--On requiert depuis ce fichier le fichier "compteur.php"-->
     <?php
         require_once 'compteur.php';
         ajouter_vue();
     ?>
     <div class="flex justify-center">
-        <div class=" bg-blue-400 mt-5 justify-center w-64 rounded-lg p-1 pl-2">
-            <span class="font-bold text-8xl"><?= nombre_vue()?></span>Visites Totales
+        <div class=" bg-blue-400 mt-5 justify-center w-auto rounded-lg p-3">
+            <!--Affichage de nombre de visiteurs-->
+            <span class="font-bold text-8xl"><?=nombre_vue()?></span>Visites Totales
         </div>
     </div>
 </body>
